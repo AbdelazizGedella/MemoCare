eba50b7012daedb5e1275d2da029f185cf83d5e1
Abdelaziz Gedella <94260690+AbdelazizGedella@users.noreply.github.com>
2025-04-25 22:24:40 +0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -16,7 +16,7 @@ function login() {
 
   firebase.auth().signInWithEmailAndPassword(email, password)
     .then((userCredential) => {
-      window.location.href = "index.html";
+      window.location.href = "dashboard.html";
     })
     .catch((error) => {
       alert("Login failed: " + error.message);
